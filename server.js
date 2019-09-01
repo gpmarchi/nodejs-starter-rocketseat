227733e717afbb3ec879b753cc2fb2cd87519da1
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2019-09-01 19:18:29 -0300

    refactoring and adding product controler

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const requireDir = require("require-dir");
 
 // iniciando o app
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 // iniciando o db
 mongoose.connect("mongodb://localhost:27017/nodeapi", {
@@ -11,16 +14,7 @@ mongoose.connect("mongodb://localhost:27017/nodeapi", {
 });
 requireDir("./src/models");
 
-const Product = mongoose.model("Product");
-
-// primeira rota
-app.get("/", (req, res) => {
-  Product.create({
-    title: "React Native",
-    description: "Build native apps with React",
-    url: "http://github.com/facebook/react-native"
-  });
-  return res.send("Hello Rocketseat");
-});
+// rotas
+app.use("/api", require("./src/routes"));
 
 app.listen(3001);
