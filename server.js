355bf38b37a0d988b1bf5948b3c3cd781e478aa5
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2019-08-31 22:57:50 -0300

    node api exercise

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
+
+// iniciando o app
+const app = express();
+
+// iniciando o db
+mongoose.connect("mongodb://localhost:27017/nodeapi", {
+  useNewUrlParser: true
+});
+requireDir("./src/models");
+
+const Product = mongoose.model("Product");
+
+// primeira rota
+app.get("/", (req, res) => {
+  Product.create({
+    title: "React Native",
+    description: "Build native apps with React",
+    url: "http://github.com/facebook/react-native"
+  });
+  return res.send("Hello Rocketseat");
+});
+
+app.listen(3001);
